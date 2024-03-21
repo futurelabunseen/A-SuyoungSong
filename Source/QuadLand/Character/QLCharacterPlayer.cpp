f2ecc8b79aca1d28e9b708f9718ef745d08ce2b5
songsuyoung <tndud3999@naver.com>
2024-03-21 18:23:12 +0900

    [Develop 1주차] 플레이어 이동 시스템 구현

---
+++ Source/QuadLand/Character/QLCharacterPlayer.cpp
@@ -0,0 +1,111 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/QLCharacterPlayer.h"
+#include "GameFramework/SpringArmComponent.h" //springArm - GameFramework
+#include "Camera/CameraComponent.h" //camera - Camera
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "QuadLand.h"
+
+AQLCharacterPlayer::AQLCharacterPlayer()
+{
+	//springArm에 Camera를 매달을 예정
+	CameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	CameraSpringArm->TargetArmLength = 450.0f;
+	CameraSpringArm->SetupAttachment(RootComponent);
+	CameraSpringArm->bUsePawnControlRotation = true; //Pawn이동에 따라서 회전 예정
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraSpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false; //spring Arm에 따라서 조절될 예정
+
+	//EnhancedInput 연결
+	static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/QuadLand/Inputs/Action/IA_Move.IA_Move'"));
+
+	if (MoveActionRef.Object)
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> LookActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/QuadLand/Inputs/Action/IA_Look.IA_Look'"));
+
+	if (LookActionRef.Object)
+	{
+		LookAction = LookActionRef.Object;
+	}
+
+	//InputContext Mapping
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> InputContextMappingRef(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/QuadLand/Inputs/IMC_Shoulder.IMC_Shoulder'"));
+
+	if (InputContextMappingRef.Object)
+	{
+		InputMappingContext = InputContextMappingRef.Object;
+	}
+}
+void AQLCharacterPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	SetCharacterControl();
+}
+
+void AQLCharacterPlayer::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AQLCharacterPlayer::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AQLCharacterPlayer::Look);
+}
+
+void AQLCharacterPlayer::SetCharacterControl()
+{
+
+	APlayerController* PlayerController = CastChecked<APlayerController>(GetController());
+	
+	if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	{
+		SubSystem->ClearAllMappings(); //모든 매핑 취소
+		UInputMappingContext* NewMappingContext = InputMappingContext;
+
+		if (NewMappingContext)
+		{
+			SubSystem->AddMappingContext(NewMappingContext,0);
+		}
+	}
+
+}
+
+void AQLCharacterPlayer::Move(const FInputActionValue& Value)
+{
+	//이동 벡터
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation(); 
+	const FRotator YawRotation(0, Rotation.Yaw, 0); //회전 방향인 Yaw만 냅둔다
+	//앞 방향 - x축
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	//옆 방향 - y축
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, MovementVector.X);
+	AddMovementInput(RightDirection, MovementVector.Y);
+}
+
+void AQLCharacterPlayer::Look(const FInputActionValue& Value)
+{
+	//마우스 시선 이동 처리
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxisVector.X);
+	AddControllerPitchInput(LookAxisVector.Y);
+}
+
+void AQLCharacterPlayer::FarmingItem()
+{
+}
+
+void AQLCharacterPlayer::Run()
+{
+}
---
+++ Source/QuadLand/QuadLand.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuadLand.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuadLand, "QuadLand" );
+
+DEFINE_LOG_CATEGORY(QLNetLog); 
+DEFINE_LOG_CATEGORY(QLLog);
